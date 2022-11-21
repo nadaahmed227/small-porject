39f63d15baac7d9994b05b37b8b0f1bc42f2a814
nadaahmed <nadaahmed227>
2022-11-21 22:03:46 +0200

    index.css

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ btnEl .addEventListener("mouseover",(Event)=>{
 //we have addEventListene And the event is the mouse 
 
 
- console.logx=(Event.pagex- btnEl.offsetLeft); //  const save value y  to subtract this  Event.pagex from  btnEl.offsetLeft
+ const x=(Event.pagex- btnEl.offsetLeft); //  const save value y  to subtract this  Event.pagex from  btnEl.offsetLeft
 const y=(Event.pagey- btnEl.offsetTop);//  const save value y to subtract this Event.pagey from  btnEl.offsetTop 
 
 btnEl.style.setproperty("--xpos",x+"px"); //We change the specified property is var(--xpos)
