45dc28ff2c06262f3edf90d742394cbe7c40aca1
nada ahmed <nadaahmed227>
2022-11-21 05:06:20 +0200

    inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const btnEl = document.querySelector(".btn"); //const: In order to return an item to that, we wrote the btn
+
+ 
+btnEl .addEventListener("mouseover",(Event)=>{ 
+//we have addEventListene And the event is the mouse 
+
+
+ const x=(Event.pagex- btnEl.offsetLeft); //  const save value y  to subtract this  Event.pagex from  btnEl.offsetLeft
+const y=(Event.pagey- btnEl.offsetTop);//  const save value y to subtract this Event.pagey from  btnEl.offsetTop 
+
+btnEl.style.setproperty("--xpos",x+"px"); //We change the specified property is var(--xpos)
+ 
+btnEl.style.setproperty("--ypos",y+"px");//We change the specified property is var(--ypos);
+
+})
