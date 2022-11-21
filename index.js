c6bf3a225d52d84ce5c0591b185959e70cb7a5bf
nadaahmed <nadaahmed227>
2022-11-21 22:53:11 +0200

    change left ,top. to var(--ypos); var(--xpos);

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const btnEl = document.querySelector(".btn"); //const: In order to return an item to that, we wrote the btn
-
+const btnEl = document.querySelector(".btn"); 
  
 btnEl .addEventListener("mouseover",(Event)=>{ 
-//we have addEventListene And the event is the mouse 
 
 
- const x=(Event.pagex- btnEl.offsetLeft); //  const save value y  to subtract this  Event.pagex from  btnEl.offsetLeft
-const y=(Event.pagey- btnEl.offsetTop);//  const save value y to subtract this Event.pagey from  btnEl.offsetTop 
 
-btnEl.style.setproperty("--xpos",x+"px"); //We change the specified property is var(--xpos)
+ const x=(Event.pagex- btnEl.offsetLeft); 
+const y=(Event.pagey- btnEl.offsetTop);
+
+btnEl.style.setproperty("--xpos",x+"px"); 
  
-btnEl.style.setproperty("--ypos",y+"px");//We change the specified property is var(--ypos);
+btnEl.style.setproperty("--ypos",y+"px");
 
 })
